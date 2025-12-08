01bcb37d3f1f5aea26773ca24cedad594bb756fc
rapharx <raphael290802@gmail.com>
2025-12-08 01:24:13 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  
+
+  return  <div className="size-32 bg-red-500"> </div>
+  
+}
+
+export default App
---
+++ src/api.ts
@@ -0,0 +1,10 @@
+import { weatherSchema } from "./schemas/weatherSchema"
+
+const API_KEY = import.meta.env.VITE_API_KEY
+
+
+export async function getWeather({lat, lon}: {lat:number, lon:number}) {
+    const res = await fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alert&appid=${API_KEY}`)
+    const data = await res.json()
+    return weatherSchema.parse(data)
+}
